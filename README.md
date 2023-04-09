bde9f59496f9691a96025541956733c434290c0e
Vahid Al <thevahidal@gmail.com>
2023-04-10 00:51:44 +0330

    Add base project

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# pun-cli
+# Pun CLI
+
+`pun-cli` is a Python-based command-line tool that generates a random pun based on a given keyword.
+
+
+## Installation
+
+You can install pun-cli via pip:
+
+```bash
+pip install pun-cli
+```
+
+## Usage
+```bash
+pun-cli [-h] [--keyword KEYWORD] [--version] [--add]
+
+Optional Arguments
+
+  --keyword (-k): A keyword to search for puns. By default, pun-cli generates a random pun.
+  --version (-v): Show the version number and exit.
+  --add (-a): Add a pun to the repository.
+```
+
+## Examples
+```bash
+$ pun-cli
+Why was the math book sad? Because it had too many problems.
+
+$ pun-cli -k cheese
+I’m reading a book about anti-gravity. It’s impossible to put down.
+```
+
+## Adding a Pun
+
+To add a pun to the repository, follow these steps:
+
+1. Fork the [pun-cli repository](https://github.com/thevahidal/pun-cli).
+2. Clone your fork to your local machine.
+3. Add your pun to pun_repository.txt file.
+4. Commit your changes and push to your fork.
+5. Create a pull request to merge your changes into the main pun-cli repository.
+
+## Contributing
+
+If you'd like to contribute to `pun-cli`, please open an issue or a pull request on the GitHub repository.
+
+## License
+
+`pun-cli` is licensed under the MIT License.
---
+++ main.py
@@ -0,0 +1,51 @@
+import random
+import argparse
+import requests
+
+PUN_REPOSITORY = 'https://raw.githubusercontent.com/thevahidal/no-pun-intended/main/pun_repository.txt'
+VERSION = 'v' + '0.1.0'
+
+def get_puns():
+    try:
+        response = requests.get(PUN_REPOSITORY)
+        puns = response.text.split('\n')
+        puns = [pun.strip() for pun in puns if pun.strip() != '']
+        return puns
+    except requests.exceptions.RequestException as e:
+        print('Error fetching puns:', e)
+        return []
+
+def generate_pun(keyword):
+    puns = get_puns()
+    if len(puns) == 0:
+        print('No puns found in repository.')
+        return ''
+
+    if keyword:
+        puns = [pun for pun in puns if keyword in pun]
+        if len(puns) == 0:
+            print('No puns found for keyword:', keyword)
+            return ''
+
+    return random.choice(puns)
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate a random pun.')
+    parser.add_argument('--keyword', '-k', help='Keyword to search for puns.')
+    parser.add_argument('--version', '-v', action='version', version='%(prog)s ' + VERSION, help='Print version and exit.')
+    parser.add_argument('--add', '-a', help='Add a pun to the repository.', action='store_true')
+
+    args = parser.parse_args()
+
+    if args.add:
+        print("Thanks for your contribution!")
+        print("Please create a pull request to add your pun to the repository.")
+        print("https://github.com/thevahidal/no-pun-intended")
+
+    else:
+      pun = generate_pun(args.keyword)
+
+      print(pun)
+
+if __name__ == '__main__':
+    main()
